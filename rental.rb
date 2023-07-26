1dc2ddecabe5b604e0f619eed2ad00f0879af665
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-07-26 19:56:31 +0100

    Add missing getters and setters

--- rental.rb
+++ rental.rb
@@ -1,8 +1,19 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, _person)
+  def initialize(date, book, person)
     @date = date
     @book = book
+    @person = person
+  end
+
+  def set_book(book)
+    @book = book
+    book.rentals << self unless book.rentals.include?(self)
+  end
+
+  def set_person(person)
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
   end
 end
